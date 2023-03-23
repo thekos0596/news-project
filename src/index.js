f6e5c4c5503026f1ba725a941b08ea0c4aa74790
SyroidNadiia <112964918+SyroidNadiia@users.noreply.github.com>
2023-03-23 11:53:25 +0200

    add fetchArticle.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
-import NewArticles from './js/fetchArticle';
-
-const newArticles = new NewArticles();
-
+import { renderArticle } from './renderArticle';
+import NewArticles from './js/API-service/api-news';
 const buttonEL = document.querySelector('.fetch');
-const newCardEl = document.querySelector('.news-card');
+const newArticles = new NewArticles();
 
 buttonEL.addEventListener('click', onFormSubmit);
 
@@ -19,56 +17,4 @@ async function onFormSubmit(event) {
   } catch (error) {
     console.log(error);
   }
-}
-
-function renderArticle(res) {
-  console.log(res.results);
-
-  const markup = res.results
-    .map(
-      ({ abstract, section, title, published_date, multimedia = [], url }) => {
-        const imageUrl =
-          multimedia && multimedia[2]?.url
-            ? multimedia[2].url
-            : './picture/image2.jpg';
-        const imageAlt =
-          multimedia && multimedia[2]?.caption
-            ? multimedia[2].caption
-            : 'Default Image';
-
-        return `
-  <ul class="news-card__image-container">
-  <li class="news-card__item">
-   <div class="news-card__foto">
-    <img src="${imageUrl}" alt="${imageAlt}" class="news-card__image">
-  <div class="news-card__category">
-   ${section}
-  </div>
-  <button class="news-card__favorite-button" data-news-id="">
-    Add to favorite
-  </button>
-  </div>
-  <div class="news-card__description">
-  <h2 class="news-card__title">
-    ${title}
-  </h2>
-  <p class="news-card__abstract">
-    ${abstract}
-  </p>
-  </div>
-  <div class="news-card__container">
-  <div class="news-card__date">
-    ${published_date}
-  </div>
-  <a href="${url}" class="news-card__read-more" data-news-id="">
-    Read more
-  </a>
-  </div>
-  </li>
-</ul>`;
-      }
-    )
-    .join('');
-
-  newCardEl.insertAdjacentHTML('beforeEnd', markup);
 }
