a812b6327eeaee01d53ed63f93747c11bd372d00
KarinaLiashko <112810602+KarinaLiashko@users.noreply.github.com>
2023-03-24 09:08:58 +0100

    click

--- src/js/search-form.js
+++ src/js/search-form.js
@@ -5,10 +5,7 @@ const closeIcon = document.querySelector(".close-icon");
 icon.onclick = () => {
     search.classList.add("active");
 }
-closeIcon.onclick = () => {
-    search.classList.remove("active");
-}
+closeIcon.addEventListener("click", () =>
+    search.classList.remove("active")
+);
 
-// closeIcon.addEventListener("click", () =>
-//         inputBox.classList.remove("active")
-//       );
