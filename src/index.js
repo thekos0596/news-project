4daddb041f0d8d12b236e8e5afa3a4cae419c40a
Volodymyr Derkach <bobikderkac@gmail.com>
2023-03-25 22:08:27 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { renderArticle } from './js/renderArticle';
 import NewArticles from './js/API-service/api-news';
 import normalization from './js/normalization.js';
 import { initAccordion } from './js/accordion';
-import addToFavorites from './js/API-service/api-btn-add_remove';
+import addToFavorites from './js/btn-add-remove';
 import './js/current-page';
 
 const buttonEL = document.querySelector('.fetch');
