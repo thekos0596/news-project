a36667c1784718bebf4a2dd1fc4b273f3117f7ee
KarinaLiashko <112810602+KarinaLiashko@users.noreply.github.com>
2023-03-25 13:04:27 +0100

    js

--- src/js/search-form.js
+++ src/js/search-form.js
@@ -1,18 +1,58 @@
+import axios from 'axios';
+
 const icon = document.querySelector('.search-box__icon');
 const search = document.querySelector('.search-box');
-const closeIcon = document.querySelector(".close-icon");
+let dataSearch = [];
 
 icon.onclick = () => {
     search.classList.add("active");
 }
-closeIcon.addEventListener("click", () =>
-    search.classList.remove("active")
-);
+// відкриття форми пошуку на мобільній версії
 
+document.addEventListener('click', (e) => {
+    const withinBoundaries = e.composedPath().includes(search);
+ 
+    if (!withinBoundaries) {
+        search.classList.remove("active") 
+    }
+})
+    // закриття форми по кліку поза формою
 
-let dataSearch = [];
+document.addEventListener('keydown', function(e) {
+	if( e.key == 27 ){ // код клавиши Escape
+		search.classList.remove("active")
+	}
+});
+// закриття форми по натисканню на Esc
 
-
-if (dataSearch.length === 0) {
-    
+open() {
+    $('body').classList.add('overflow__hidden')
 }
+// блокую body щоб не скролився
+
+
+export class SearchModel{
+start () {
+    if (localStorage.getItem("search-data") === null) {
+// перевіряю якщо масив пустий тоді виконується функція
+
+    axios.get(`/search`).then((r) => {
+     
+        localStorage.setItem("search-data", JSON.stringify(r.data))
+// дані перероблюю на стрічку
+        dataSearch = JSON.parse(localStorage.getItem("search-data"))
+// в масив кладу дані із локалсторедж
+    }).catch(r => {
+       
+        CustomPush.show({
+            title:r,
+        })
+    });
+} else {
+    dataSearch = JSON.parse(localStorage.getItem("search-data"))
+// якщо дані в локалсторедж є, то отримую їх без запиту 
+    }
+    }
+}
+
+
