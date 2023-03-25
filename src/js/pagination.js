a3fa867e8fd18cc3421aa6930607124acf8008ef
Oleg Kirpa <profix.ok@gmail.com>
2023-03-25 08:06:49 +0200

    pagin

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,41 +1,134 @@
 import NewArticles from './API-service/api-news';
 import { renderArticle } from '../renderArticle';
 
-const paginationEl = document.getElementById('#pagination');
-const arrowLeft = document.querySelector('.arrow_left');
-const arrowRight = document.querySelector('.arrow_right');
+const pg = document.getElementById('pagination');
+const btnPrevPg = document.querySelector('.arrow-prew');
+const btnNextPg = document.querySelector('.arrow-next');
 
-let currentPage = 1;
+const valuePage = {
+  curPage: 1,
+  numLinksTwoSide: 1,
+  totalPages: 20,
+};
 
-const newArticles = new NewArticles();
+pagination();
 
-arrowLeft.addEventListener('click', onFormSubmit);
+pg.addEventListener('click', e => {
+  const ele = e.target;
+  console.log('ele:', ele);
 
-async function onFormSubmit(event) {
-  event.preventDefault();
+  if (ele.dataset.page) {
+    const pageNumber = parseInt(e.target.dataset.page);
 
-  try {
-    const res = await newArticles.fetchArtic();
+    valuePage.curPage = pageNumber;
+    pagination(valuePage);
+    console.log(valuePage);
+    handleButtonLeft();
+    handleButtonRight();
+  }
+});
 
-    console.log(res);
+// DYNAMIC PAGINATION
+function pagination() {
+  const { totalPages, curPage, numLinksTwoSide: delta } = valuePage;
 
-    renderArticle(res);
-  } catch (error) {
-    console.log(error);
+  const range = delta + 4; // use for handle visible number of links left side
+
+  let render = '';
+  let renderTwoSide = '';
+  let dot = `<li class="pg-item"><a class="pg-link">...</a></li>`;
+  let countTruncate = 0; // use for ellipsis - truncate left side or right side
+
+  // use for truncate two side
+  const numberTruncateLeft = curPage - delta;
+  const numberTruncateRight = curPage + delta;
+
+  let active = '';
+  for (let pos = 1; pos <= totalPages; pos++) {
+    active = pos === curPage ? 'active' : '';
+
+    // truncate
+    if (totalPages >= 2 * range - 1) {
+      if (numberTruncateLeft > 3 && numberTruncateRight < totalPages - 3 + 1) {
+        // truncate 2 side
+        if (pos >= numberTruncateLeft && pos <= numberTruncateRight) {
+          renderTwoSide += renderPage(pos, active);
+        }
+      } else {
+        // truncate left side or right side
+        if (
+          (curPage < range && pos <= range) ||
+          (curPage > totalPages - range && pos >= totalPages - range + 1) ||
+          pos === totalPages ||
+          pos === 1
+        ) {
+          render += renderPage(pos, active);
+        } else {
+          countTruncate++;
+          if (countTruncate === 1) render += dot;
+        }
+      }
+    } else {
+      // not truncate
+      render += renderPage(pos, active);
+    }
+  }
+
+  if (renderTwoSide) {
+    renderTwoSide =
+      renderPage(1) + dot + renderTwoSide + dot + renderPage(totalPages);
+    pg.innerHTML = renderTwoSide;
+  } else {
+    pg.innerHTML = render;
   }
 }
 
-// неактивні кнопки prew або next
-// function disableArrowBtn(totalPages) {
-//   if (currentPage === 1) {
-//     arrowLeft.classList.add('disabled-arrow');
-//   } else {
-//     arrowLeft.classList.remove('disabled-arrow');
-//   }
+function renderPage(index, active = '') {
+  return ` <li class="pg-item ${active}" data-page="${index}">
+        <a class="pg-link" href="#">${index}</a>
+    </li>`;
+}
 
-//   if (currentPage === totalPages) {
-//     arrowRight.classList.add('disabled-arrow');
-//   } else {
-//     arrowRight.classList.remove('disabled-arrow');
-//   }
-// }
+document
+  .querySelector('.page-container')
+  .addEventListener('click', function (e) {
+    handleButton(e.target);
+  });
+
+function handleButton(element) {
+  if (element.classList.contains('first-page')) {
+    valuePage.curPage = 1;
+  } else if (element.classList.contains('last-page')) {
+    valuePage.curPage = 20;
+  } else if (element.classList.contains('prev-page')) {
+    valuePage.curPage--;
+    handleButtonLeft();
+    btnNextPg.disabled = false;
+    btnLastPg.disabled = false;
+  } else if (element.classList.contains('next-page')) {
+    valuePage.curPage++;
+    handleButtonRight();
+    btnPrevPg.disabled = false;
+    btnFirstPg.disabled = false;
+  }
+  pagination();
+}
+function handleButtonLeft() {
+  if (valuePage.curPage === 1) {
+    btnPrevPg.disabled = true;
+    btnFirstPg.disabled = true;
+  } else {
+    btnPrevPg.disabled = false;
+    btnFirstPg.disabled = false;
+  }
+}
+function handleButtonRight() {
+  if (valuePage.curPage === valuePage.totalPages) {
+    console.log(valuePage.curPage);
+    btnNextPg.disabled = true;
+    btnLastPg.disabled = true;
+  } else {
+    btnNextPg.disabled = false;
+    btnLastPg.disabled = false;
+  }
+}
