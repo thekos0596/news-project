34bf66efddf5e7e55626a26ee1a0cbb710c8f2cc
KarinaLiashko <112810602+KarinaLiashko@users.noreply.github.com>
2023-03-26 21:28:33 +0200

    js

--- src/js/search-form.js
+++ src/js/search-form.js
@@ -8,7 +8,7 @@ import renderSearchNews from './renderSerchNews';
 const newArticles = new NewArticles();
 
 const btnAddtoFavEl = document.querySelector('.news-card');
-const icon = document.querySelector('.search-box__icon');
+const icon = document.querySelector('.search-box__icon-svg');
 const search = document.querySelector('.search-box');
 
 search.addEventListener('submit', onFormSubmit);
@@ -28,17 +28,17 @@ async function onFormSubmit(e) {
   }
 }
 
-function messageInfo(arr) {
-  if (arr.input.value === 0) {
-    return `<h2 class="message-info">We haven’t found news from this category</h2>
-    <img src="${defImg}" class="defImg"/>`;
-  }
-  // повинна підгружатись картинка
+// function messageInfo(arr) {
+//   if (arr.input.value === 0) {
+//     return `<h2 class="message-info">We haven’t found news from this category</h2>
+//     <img src="${defImg}" class="defImg"/>`;
+//   }
+//   // повинна підгружатись картинка
 
-  if (arr.totalInput.value !== 0) {
-    Notify.success(`Hooray! We found ${arr.totalInput.value} articles.`);
-  }
-}
+//   if (arr.totalInput.value !== 0) {
+//     Notify.success(`Hooray! We found ${arr.totalInput.value} articles.`);
+//   }
+// }
 
 icon.onclick = () => {
   search.classList.add('active');
