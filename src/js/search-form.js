8191b713158cccfb0c316133d27afceb34eaeaa3
KarinaLiashko <112810602+KarinaLiashko@users.noreply.github.com>
2023-03-26 16:03:49 +0200

    js

--- src/js/search-form.js
+++ src/js/search-form.js
@@ -1,27 +1,19 @@
-import { fetchArtic } from './API-service/api-news';
-import { API_KEY } from './API-service/api-news';
-import { BASE_URL } from './API-service/api-news';
+
 import axios from 'axios';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import { log } from 'console';
-
 import { renderArticle } from './renderArticle';
 import NewArticles from './API-service/api-news';
 import normalization from './normalization.js';
 import { normalizationSearch } from './normalization.js';
+import defImg from '../images/defaultimage.jpg';
+
 const newArticles = new NewArticles();
 const btnAddtoFavEl = document.querySelector('.news-card');
-
 const icon = document.querySelector('.search-box__icon');
 const search = document.querySelector('.search-box');
 const input = document.querySelector('#mySearch');
 
 console.log(search.elements.value);
-let page = 1;
-let title = ' ';
-const perPage = 7;
-
-let cardNews = [];
 
 input.addEventListener('input', onFormSubmit);
 
@@ -37,40 +29,15 @@ async function onFormSubmit(e) {
   } catch (error) {
     console.log(error);
   }
+
 }
 
-// async function onSubmitNews(e) {
-//   e.preventDefault();
-//   console.log('test', e.currentTarget.value);
 
-//   title = e.target.elements.searchQuery.value.trim();
-//   if (input.value === '' && input.value.lendth <= 1) {
-//     return Notify.failure(
-//       'Sorry, the search field cannot be empty. Please enter information to search.'
-//     );
-//   }
 
-//   const { data } = await searchingNews(title);
-
-//   articleCard(data);
-//   // сюди будуть додаватись знайдені статті
-//   messageInfo(data);
-//   stopSearch(data);
-//   e.target.reset();
-
-//   try {
-//     const res = await newArticles.fetchArtic(searchNews);
-//     const normalizedResults = normalization(res);
-//     renderArticle(normalizedResults);
-//   } catch (error) {
-//     console.log(error);
-//   }
-// }
-import defImg from '../images/defaultimage.jpg';
 function messageInfo(arr) {
   if (arr.input.value === 0) {
     return `<h2 class="message-info">We haven’t found news from this category</h2>
-    <img src="${defImg}"/>`;
+    <img src="${defImg}" class="defImg"/>`;
   }
   // повинна підгружатись картинка
 
