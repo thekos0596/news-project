55df76c417787f791909609abc5e5e8299f0c5ca
VolodymyrDerkach97 <bobikderkac@gmail.com>
2023-03-27 11:11:27 +0300

    Update new-cards.js

--- src/js/new-cards.js
+++ src/js/new-cards.js
@@ -3,7 +3,7 @@ import addToFavorites from './btn-add-remove';
 import readMore from './btn-read-more';
 
 const buttonEL = document.querySelector('.fetch');
-const newArticles = new NewArticles();
+// const newArticles = new NewArticles();
 const newsCardEl = document.querySelector('.news-card');
 
 buttonEL.addEventListener('click', onFormSubmit);
@@ -17,18 +17,18 @@ newsCardEl.addEventListener('click', function (event) {
   }
 });
 
-const numCardsOnPages = 9;
+// const numCardsOnPages = 9;
 
-async function onFormSubmit(event) {
-  event.preventDefault();
+// async function onFormSubmit(event) {
+//   event.preventDefault();
 
-  try {
-    const res = await newArticles.fetchArtic();
-    const normalizedResults = normalization(res);
-    const newArray = normalizedResults.slice(0, numCardsOnPages);
-    btnAddtoFavEl.innerHTML = '';
-    renderArticle(newArray);
-  } catch (error) {
-    console.log(error);
-  }
-}
+//   try {
+//     const res = await newArticles.fetchArtic();
+//     const normalizedResults = normalization(res);
+//     const newArray = normalizedResults.slice(0, numCardsOnPages);
+//     btnAddtoFavEl.innerHTML = '';
+//     renderArticle(newArray);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
