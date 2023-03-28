810b1eec31d815fe120455632f8bf2c90a71dd07
Victoriia Miroshnikova <victoriia.pl@gmail.com>
2023-03-28 17:00:43 +0300

    favorite

--- src/favorite.js
+++ src/favorite.js
@@ -1,5 +1,7 @@
+import './js/add-to-favorite';
 import './js/current-page';
 import './js/toggle-switcher-theme';
 import './js/search-form';
 import './js/formOpen';
 import './js/formRegFatchData';
+import './js/to-favorite';
