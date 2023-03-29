1500a644ec5444960302756450f6505a45250c68
Maryna-Harasko <mngarasko@gmail.com>
2023-03-29 17:50:06 +0300

    fixed favorite list

--- src/js/render-ac-card.js
+++ src/js/render-ac-card.js
@@ -3,10 +3,25 @@ import svgSprite from '../images/icons/icons.svg';
 import defImgPng from '../images/default_hidden.png';
 import defImg from '../images/defaultimage.jpg';
 import './btn-add-remove';
+import { checkFavorites } from './btn-add-remove';
 
 const containerEl = document.querySelector('.container__read');
 const newAccorEl = document.querySelector('.accordion');
 const response = JSON.parse(localStorage.getItem('readList')) || [];
+const favoriteList = JSON.parse(localStorage.getItem('favoriteList')) || [];
+
+containerEl.addEventListener('click', function (event) {
+  checkFavorites(favoriteList);
+});
+containerEl.addEventListener('click', function (event) {
+  const targetEl = event.target;
+  if (targetEl.classList.contains('news-card__favorite-button')) {
+    console.log('this is favorire');
+    addToFavorites(event);
+  } else if (targetEl.classList.contains('news-card__read-more')) {
+    console.log('news-card__read-more');
+  }
+});
 
 export function pageEmpty(){
   if (!response || response.length === 0){
@@ -45,7 +60,7 @@ function newFormatArray(response){
 const formattedItems = newFormatArray(response);
 
 export function renderAccordion(){
-  containerEl.insertAdjacentHTML('beforeEnd', pageEmpty());
+  // containerEl.insertAdjacentHTML('beforeEnd', pageEmpty());
 
   const uniqueDates = new Set();
  
@@ -82,6 +97,10 @@ export function renderAccordion(){
 renderAccordion();
 
 function markupReadCard({ abstract, section, title, published_date, imageUrl, imageAlt, url }) {
+  let newsId = [];
+  const bool = newsId.includes(title.toUpperCase());
+  const articleTitle = bool ? 'Remove from favorite' : 'Add to favorite';
+  const iconClass = bool ? 'icon-favorite' : 'icon-heart';
   return `
     <li class="news-card__item">
       <div class="news-card__foto">
@@ -89,9 +108,8 @@ function markupReadCard({ abstract, section, title, published_date, imageUrl, im
         <div class="news-card__category">
         ${section}
         </div>
-        <button class="news-card__favorite-button" data-news-id="${title}">
-        Add to favorite <svg width="13" height="12" class="news-card__favorite-icon">
-        <use href="${svgSprite}#icon-favorite"></use></svg>
+        <button class="news-card__favorite-button" data-news-id="${title}">${articleTitle}<svg width="13" height="12" class="news-card__favorite-icon">
+        <use href="${svgSprite}#${iconClass}"></use></svg>
          </button>
       </div>
       <div class="news-card__description">
@@ -133,7 +151,6 @@ function addMarkupToCards() {
     
     items.forEach((item) => {
       if (item.readAt === date) {
-        console.log(item)
         cardContainer.insertAdjacentHTML('beforeEnd', markupReadCard(item));
       }
     });
@@ -141,3 +158,45 @@ function addMarkupToCards() {
 }
 
 addMarkupToCards()
+
+
+function createIcon(bool, btn) {
+  const icon = !bool ? `${svgSprite}#icon-favorite` : `${svgSprite}#icon-heart`;
+  const svg = document.createElement('svg');
+  svg.classList.add('news-card__favorite-icon');
+  svg.setAttribute('width', '13');
+  svg.setAttribute('height', '12');
+  const use = document.createElement('use');
+  use.setAttribute('href', icon);
+  svg.appendChild(use);
+  btn.insertAdjacentHTML('beforeend', svg.outerHTML);
+}
+
+
+
+function addToFavorites(event) {
+  const btn = event.target.closest('.news-card__favorite-button');
+
+  const newsId = btn.dataset.newsId;
+
+  const readList = JSON.parse(localStorage.getItem('readList')) || [];
+  const favoriteList = JSON.parse(localStorage.getItem('favoriteList')) || [];
+ 
+  const favoriteIndex = favoriteList.findIndex(
+    favorite => favorite.title === newsId
+  );
+  
+  const bool = favoriteIndex === -1;
+  if (bool) {
+    const currentNews = readList.find(news => news.title === newsId);
+    favoriteList.push(currentNews);
+    
+  } else {
+    favoriteList.splice(favoriteIndex, 1);
+  }
+
+  localStorage.setItem('favoriteList', JSON.stringify(favoriteList));
+
+  btn.textContent = bool ? 'Remove from favorite' : 'Add to favorite';
+  createIcon(bool, btn);
+}
